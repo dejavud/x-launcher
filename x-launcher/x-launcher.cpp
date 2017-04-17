5a3caec49ba26fdd2497fc8cfade225ea6f8433b
dejavud <dejavud@qq.com>
2017-04-17 22:09:40 +0800

    init project

---
+++ x-launcher/x-launcher.cpp
@@ -0,0 +1,33 @@
+// x-launcher.cpp : main source file for x-launcher.exe
+//
+
+#include "stdafx.h"
+#include "resource.h"
+
+
+CAppModule _Module;
+
+int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR /*lpstrCmdLine*/, int /*nCmdShow*/)
+{
+	HRESULT hRes = ::CoInitialize(NULL);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+	::DefWindowProc(NULL, 0, 0, 0L);
+
+	AtlInitCommonControls(ICC_BAR_CLASSES);	// add flags to support other controls
+
+	hRes = _Module.Init(NULL, hInstance);
+	ATLASSERT(SUCCEEDED(hRes));
+
+	int nRet = 0;
+	// BLOCK: Run application
+	{
+		
+	}
+
+	_Module.Term();
+	::CoUninitialize();
+
+	return nRet;
+}
---
+++ x-launcher/x-launcher.h
@@ -0,0 +1,1 @@
+// x-launcher.h
